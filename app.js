f1a70468c098cb15019824b99b5dd505ead32693
David Aderaldo <119186007+davidade300@users.noreply.github.com>
2023-09-27 12:27:55 -0300

    adicionando consoles log

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("A Aplicação está se conectando ao banco de dados")
+// mais um console log de teste
+console.log("Esse commit foi feito por um terceiro")
+console.log("Enviando para o GitHub")
